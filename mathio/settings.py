34aa1a0ce41d19fb86a4949d8839aebf2d1dd256
Paul Liao <liaoyunyang0815@gmail.com>
2024-08-14 15:42:50 +0800

    enhance security

--- mathio/settings.py
+++ mathio/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9_5&=qqv$!=+z%jw&+66zoru!!$7#b2#=ak7z-ina*dna+wf$5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
+
+
+
 
 
 # Application definition
@@ -54,6 +57,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+SECRET_KEY = os.environ.get("SECRET_KEY", "")
 
 ROOT_URLCONF = 'mathio.urls'
 
